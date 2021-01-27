ee9b2297b91e94122a66fd3a0e2dd943893a96d7
jtreeves <jackson.t.reeves@gmail.com>
2021-01-26 21:31:22 -0500

    [Home] Add form to take in data from user

--- src/components/pages/profile/Home.js
+++ src/components/pages/profile/Home.js
@@ -1,11 +1,88 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+import FormGroup from '../../elements/main/FormGroup'
+
+const REGRESSIONZ_API_KEY = process.env.REGRESSIONZ_API_KEY
+const regressionz = 'https://regressionz.herokuapp.com/api'
+
 // Create function
 function Profile(props) {
+    // Set initial state values
+    const [source, setSource] = useState('')
+    const [title, setTitle] = useState('')
+    const [independent, setIndependent] = useState('')
+    const [dependent, setDependent] = useState('')
+    const [dataSet, setDataSet] = useState([])
+    
+    // Set source for form
+    const handleSource = (e) => {
+        setSource(e.target.value)
+    }
+
+    // Set title for form
+    const handleTitle = (e) => {
+        setTitle(e.target.value)
+    }
+
+    // Set independent for form
+    const handleIndependent = (e) => {
+        setIndependent(e.target.value)
+    }
+
+    // Set dependent for form
+    const handleDependent = (e) => {
+        setDependent(e.target.value)
+    }
+
+    // Set dataSet for form
+    const handleDataSet = (e) => {
+        setDataSet(e.target.value)
+    }
+
     return (
         <div>
             <h1>Profile</h1>
             <p><strong>Name:</strong> {props.user.name}</p> 
             <p><strong>Email:</strong> {props.user.email}</p> 
             <p><strong>ID:</strong> {props.user.id}</p>
+            <form onSubmit={handleSubmit}>
+                <FormGroup 
+                    type="hidden" 
+                    label="source" 
+                    value={source}
+                    onChange={handleSource} 
+                />
+                <FormGroup
+                    type="text"
+                    label="title"
+                    value={title}
+                    display="Title"
+                    onChange={handleTitle}
+                />
+                <FormGroup
+                    type="text"
+                    label="independent"
+                    value={independent}
+                    display="Independent"
+                    onChange={handleIndependent}
+                />
+                <FormGroup
+                    type="text"
+                    label="dependent"
+                    value={dependent}
+                    display="Dependent"
+                    onChange={handleDependent}
+                />
+                <FormGroup
+                    type="text"
+                    label="dataSet"
+                    value={dataSet}
+                    display="Data Set"
+                    onChange={handleDataSet}
+                />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
