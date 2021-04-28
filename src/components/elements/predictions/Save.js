4a14f727ca94a4a25f0540a7e5c59bbb4a199bd3
jtreeves <jackson.t.reeves@gmail.com>
2021-04-28 19:21:35 -0400

    [Save] Add link to save prediction models

--- src/components/elements/predictions/Save.js
+++ src/components/elements/predictions/Save.js
@@ -1,19 +1,27 @@
-import FormGroup from '../main/FormGroup'
+import axios from 'axios'
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
 
 function Save(props) {
+    const source = props.source
+    const sections = {
+        favorite: {},
+        note: {}
+    }
+
+    const handleSubmit = async (e) => {
+        try {
+            e.preventDefault()
+            await axios.post(REACT_APP_SERVER_URL + 'predictions/' + props.user.id, { source, sections })
+            alert('Models saved!')
+        } catch(error) {
+            alert(error.response.data.msg)
+            console.log(`SAVE PREDICTION ERROR: ${error}`)
+        }
+    }
+
     return (
         <div>
-            <form onSubmit={props.handleSubmit}>
-                <FormGroup
-                    type="hidden"
-                    label="user"
-                    value={props.user}
-                />
-                <FormGroup
-                    type="hidden"
-                    label="source"
-                    value={props.source}
-                />
+            <form onSubmit={handleSubmit}>
                 <button type="submit">Save</button>
             </form>
         </div>
