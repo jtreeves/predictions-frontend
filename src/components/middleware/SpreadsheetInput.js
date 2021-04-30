17f45d3e060b2032b035601b61c0a6b9dcc677fa
jtreeves <jackson.t.reeves@gmail.com>
2021-04-30 14:19:18 -0400

    [SpreadsheetInput] Change function to render results as a string on a single line with elements enclosed in brackets

--- src/components/middleware/SpreadsheetInput.js
+++ src/components/middleware/SpreadsheetInput.js
@@ -7,7 +7,31 @@ function SpreadsheetInput() {
 		const reader = new FileReader()
         reader.onload = (e) => {
             const data = e.target.result
-            output.textContent = data
+            let collection = data
+            const returns = [...collection.matchAll('\r\n')]
+            const indices = [0]
+            for (const array of returns) {
+                indices.push(array.index)
+                indices.push(array.index + 2)
+            }
+            indices.push(collection.length)
+            let slices = []
+            for (let i = 0; i < indices.length - 1; i++) {
+                slices.push(collection.slice(indices[i], indices[i + 1]))
+            }
+            let trimmedSlices = []
+            for (let i = 0; i < slices.length; i++) {
+                if (i%2 === 0) {
+                    trimmedSlices.push(slices[i])
+                }
+            }
+            let stringedList = ''
+            for (const slice of trimmedSlices) {
+                stringedList += '[' + slice + '],'
+            }
+            const trimmedString = stringedList.slice(0, -1)
+            const final = '[' + trimmedString + ']'
+            output.textContent = final
         }
         reader.readAsText(file)
     }
