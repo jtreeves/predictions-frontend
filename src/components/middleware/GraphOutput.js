b403c01f760a89290ceffa4bfb537acca59f7837
jtreeves <jackson.t.reeves@gmail.com>
2021-04-30 18:39:17 -0400

    [GraphOutput] Add three points to the graph

--- src/components/middleware/GraphOutput.js
+++ src/components/middleware/GraphOutput.js
@@ -2,6 +2,12 @@ import { useEffect } from 'react'
 import * as d3 from 'd3'
 
 function GraphOutput() {
+    const points = [
+        { x: 10, y: 20 },
+        { x: 30, y: 40 },
+        { x: 80, y: 90 }
+    ]
+
     useEffect(() => {
         const margin = { top: 50, right: 50, bottom: 50, left: 50 }
         const width = 500 - margin.left - margin.right
@@ -26,7 +32,16 @@ function GraphOutput() {
         svg
             .append("g")
             .call(d3.axisLeft(y))
-    }, [])
+
+        svg
+            .selectAll("plot")
+            .data(points)
+            .enter()
+            .append("circle")
+            .attr("cx", (d) => x(d.x))
+            .attr("cy", (d) => y(d.y))
+            .attr("r", 5)
+    })
 
     return (
         <svg
