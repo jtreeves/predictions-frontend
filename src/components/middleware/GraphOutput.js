8bac5538141ef31ef0b0f3202e5cd124e966d678
jtreeves <jackson.t.reeves@gmail.com>
2021-04-30 17:22:14 -0400

    [GraphOutput] Display three circles in different colors

--- src/components/middleware/GraphOutput.js
+++ src/components/middleware/GraphOutput.js
@@ -3,21 +3,33 @@ import * as d3 from 'd3'
 
 function GraphOutput() {
     useEffect(() => {
-        d3.select("#graph")
+        const svg = d3.select("#graph")
+        svg
+            .append("circle")
+            .attr("cx", 100)
+            .attr("cy", 100)
+            .attr("r", 50)
+            .style("fill", "blue")
+        svg
+            .append("circle")
+            .attr("cx", 200)
+            .attr("cy", 200)
+            .attr("r", 50)
+            .style("fill", "red")
+        svg
+            .append("circle")
+            .attr("cx", 300)
+            .attr("cy", 300)
+            .attr("r", 50)
+            .style("fill", "green")
     }, [])
 
     return (
-        <svg>
-            <circle
-                id="graph"
-                fill="green"
-                stroke="black"
-                strokeWidth={5}
-                cx={100}
-                cy={100}
-                r={25}
-            />
-        </svg>
+        <svg
+            id="graph"
+            height={500}
+            width={500}
+        />
     )
 }
 
