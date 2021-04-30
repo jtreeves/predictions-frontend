2f87b0fda31fb6b924234aae5453e6642d7a7bb2
jtreeves <jackson.t.reeves@gmail.com>
2021-04-30 18:01:40 -0400

    [GraphOut] Display x- and y- axes

--- src/components/middleware/GraphOutput.js
+++ src/components/middleware/GraphOutput.js
@@ -3,25 +3,29 @@ import * as d3 from 'd3'
 
 function GraphOutput() {
     useEffect(() => {
-        const svg = d3.select("#graph")
+        const margin = { top: 50, right: 50, bottom: 50, left: 50 }
+        const width = 500 - margin.left - margin.right
+        const height = 500 - margin.top - margin.bottom
+
+        const svg = d3
+            .select("#graph")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", `translate(${margin.left}, ${margin.top})`)
+        
+        const x = d3.scaleLinear().domain([0, 100]).range([0, width])
+        const y = d3.scaleLinear().domain([0, 100]).range([height, 0])
+
         svg
-            .append("circle")
-            .attr("cx", 100)
-            .attr("cy", 100)
-            .attr("r", 50)
-            .style("fill", "blue")
+            .append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(x))
+
         svg
-            .append("circle")
-            .attr("cx", 200)
-            .attr("cy", 200)
-            .attr("r", 50)
-            .style("fill", "red")
-        svg
-            .append("circle")
-            .attr("cx", 300)
-            .attr("cy", 300)
-            .attr("r", 50)
-            .style("fill", "green")
+            .append("g")
+            .call(d3.axisLeft(y))
     }, [])
 
     return (
