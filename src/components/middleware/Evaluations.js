bc4ed7bba4ee5668178aa5a7a8882221ea362378
jtreeves <jackson.t.reeves@gmail.com>
2021-05-03 21:22:17 -0400

    [Evaluations] Add function to evaluate all major functions at any numeral input

---
+++ src/components/middleware/Evaluations.js
@@ -0,0 +1,25 @@
+function Evaluations(equationType, coefficients, independent) {
+    let dependent = 0
+
+    if (equationType === 'linear') {
+        dependent += coefficients[0] * independent + coefficients[1]
+    } else if (equationType === 'quadratic') {
+        dependent += coefficients[0] * independent**2 + coefficients[1] * independent + coefficients[2]
+    } else if (equationType === 'cubic') {
+        dependent += coefficients[0] * independent**3 + coefficients[1] * independent**2 + coefficients[2] * independent + coefficients[3]
+    } else if (equationType === 'hyperbolic') {
+        dependent += coefficients[0] / independent + coefficients[1]
+    } else if (equationType === 'exponential') {
+        dependent += coefficients[0] * coefficients[1]**independent
+    } else if (equationType === 'logarithmic') {
+        dependent += coefficients[0] * Math.log(independent) + coefficients[1]
+    } else if (equationType === 'logistic') {
+        dependent += coefficients[0] / (1 + Math.exp(-1 * coefficients[1] * (independent - coefficients[2])))
+    } else if (equationType === 'sinusoidal') {
+        dependent += coefficients[0] * Math.sin(coefficients[1] * (independent - coefficients[2])) + coefficients[3]
+    }
+    
+    return dependent
+}
+
+export default Evaluations
