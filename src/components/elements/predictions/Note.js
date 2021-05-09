5b4d9d87e8d79f720aa576be562ab45c8faae5e0
jtreeves <jackson.t.reeves@gmail.com>
2021-05-09 19:40:58 -0400

    [Note] Refactor to make textarea appear on all renders, like with dataSet below

--- src/components/elements/predictions/Note.js
+++ src/components/elements/predictions/Note.js
@@ -6,16 +6,10 @@ const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
 function Note(props) {
     const source = props.source
     const [note, setNote] = useState(props.note)
-    const [changing, setChanging] = useState(false)
     const [submitting, setSubmitting] = useState(false)
 
     const handleNote = (e) => {
         setNote(e.target.value)
-    }
-
-    const handleChanging = (e) => {
-        e.preventDefault()
-        setChanging(true)
     }
 
     const handleSubmitting = async (e) => {
@@ -28,63 +22,26 @@ function Note(props) {
         }
     }
 
-    if (note === '') {
-        if (!changing) {
-            return (
-                <section>
-                    <mark>Note</mark>
-                    <p>You have not added a note yet.</p>
-                    <button onClick={handleChanging}>Add Note</button>
-                </section>
-            )
-        } else {
-            return (
-                <section>
-                    <mark>Note</mark>
-                    <form onSubmit={handleSubmitting}>
-                        <FormGroup
-                            type="textarea"
-                            label="note"
-                            value={note}
-                            display="Provide some details:"
-                            tooltip="Add a note to save your thoughts about this data set"
-                            onChange={handleNote}
-                        />
-                        <button type="submit">Save Note</button>
-                    </form>
-                </section>
-            )
-        }
+    if (!submitting) {
+        return (
+            <section>
+                <mark>Note</mark>
+                <form onSubmit={handleSubmitting}>
+                    <FormGroup
+                        type="textarea"
+                        label="note"
+                        value={note}
+                        display="Provide some details:"
+                        tooltip="Add a note to save your thoughts about this data set"
+                        onChange={handleNote}
+                    />
+                    <button type="submit">Save Note</button>
+                </form>
+            </section>
+        )
     } else {
-        if (!changing) {
-            return (
-                <section>
-                    <mark>Note</mark>
-                    <p>{note}</p>
-                    <button onClick={handleChanging}>Update Note</button>
-                </section>
-            )
-        } else if (!submitting) {
-            return (
-                <section>
-                    <mark>Note</mark>
-                    <form onSubmit={handleSubmitting}>
-                        <FormGroup
-                            type="textarea"
-                            label="note"
-                            value={note}
-                            display="Provide more details:"
-                            tooltip="Add even more of your thoughts about this data set"
-                            onChange={handleNote}
-                        />
-                        <button type="submit">Save Note</button>
-                    </form>
-                </section>
-            )
-        } else {
-            setChanging(false)
-            setSubmitting(false)
-        }
+        alert('Your note was saved!')
+        setSubmitting(false)
     }
 }
 
