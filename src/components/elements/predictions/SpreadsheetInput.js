202fa29943589cff32bfc9ff3c6f4c3d23622537
jtreeves <jackson.t.reeves@gmail.com>
2021-05-10 11:56:54 -0400

    [SpreadsheetInput] Try implementing useRef to mask and style the upload button for an input with a type of file

--- src/components/elements/predictions/SpreadsheetInput.js
+++ src/components/elements/predictions/SpreadsheetInput.js
@@ -1,11 +1,21 @@
 import CreateSet from './CreateSet'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 function SpreadsheetInput(props) {
+    // const [upload, setUpload] = useState(false)
     const fileToUpload = useRef(null)
+    const handleBrowse = (e) => {
+        console.log('INSIDE HANDLE BROWSE')
+        // e.preventDefault()
+        fileToUpload.current.click()
+        // setUpload(true)
+    }
     const handleSpreadsheet = (e) => {
-        e.preventDefault()
-        const file = document.querySelector("#csv").files[0]
+        console.log('INSIDE HANDLE SPREADSHEET')
+        // e.preventDefault()
+        // fileToUpload.current.click()
+        // const file = document.querySelector("#csv").files[0]
+        const file = e.target.files[0]
         console.log('FILE IN HANDLE: ', file)
 		const reader = new FileReader()
         reader.onload = (e) => {
@@ -38,29 +48,50 @@ function SpreadsheetInput(props) {
         reader.readAsText(file)
     }
 
+    // if (!upload) {
+    //     return (
+    //         <form>
+    //             <input type="file" id="csv" 
+    //             ref={fileToUpload}
+    //             style={{display: 'none'}} 
+    //             />
+    //             <button
+    //                 onClick={handleBrowse}
+    //             >Browse</button>
+    //             {/* <button 
+    //                 id="upload" 
+    //                 onClick={handleSpreadsheet}
+    //                 className="submission"
+    //             >
+    //                 Browse
+    //             </button> */}
+    //         </form>
+    //     )
+    // } else
     if (props.dataSet === '') {
         return (
             <form>
                 <input type="file" id="csv" 
-                // ref={fileToUpload}
-                // style={{display: 'none'}} 
+                ref={fileToUpload}
+                style={{display: 'none'}} 
+                onChange={handleSpreadsheet}
                 />
-                {/* <button
-                    onClick={() => fileToUpload.current.click()}
-                >Browse</button> */}
-                <button 
+                <button
+                    onClick={handleBrowse}
+                >Upload</button>
+                {/* <button 
                     id="upload" 
                     onClick={handleSpreadsheet}
                     className="submission"
                 >
-                    Upload Data
-                </button>
+                    Upload
+                </button> */}
             </form>
         )
     } else {
         return (
             <CreateSet 
-                heading="Create a New Data Set"
+                // heading="Create a New Data Set"
                 handleSubmit={props.handleSubmit}
                 title={props.title}
                 handleTitle={props.handleTitle}
@@ -73,6 +104,7 @@ function SpreadsheetInput(props) {
                 dataSet={props.dataSet}
                 handleDataSet={props.handleDataSet}
                 stored={false}
+                initiated={false}
                 button="Create Set"
             />
         )
--- src/components/pages/main/Welcome.js
+++ src/components/pages/main/Welcome.js
@@ -1,11 +1,63 @@
 import '../../../style/welcome.css'
+import { useRef } from 'react'
 
 // Create function
 function Welcome() {
+    const inputFile = useRef(null)
+    const handleClick = (e) => {
+        console.log('INSIDE HANDLE CLICK')
+        // e.preventDefault()
+        inputFile.current.click()
+
+    }
+    const handleFile = (e) => {
+        console.log('INSIDE HANDLE FILE')
+        // e.preventDefault()
+        const file = e.target.files[0]
+        console.log('FILE: ', file)
+        const reader = new FileReader()
+        reader.onload = (e) => {
+            const data = e.target.result
+            const results = document.querySelector('#results')
+            results.innerText = data
+            // const newLines = [...data.matchAll('\r\n')]
+            // const indices = [0]
+            // for (const newLine of newLines) {
+            //     indices.push(newLine.index)
+            //     indices.push(newLine.index + 2)
+            // }
+            // indices.push(data.length)
+            // let dataSections = []
+            // for (let i = 0; i < indices.length - 1; i++) {
+            //     dataSections.push(data.slice(indices[i], indices[i + 1]))
+            // }
+            // let points = []
+            // for (let i = 0; i < dataSections.length; i++) {
+            //     if (i%2 === 0) {
+            //         points.push(dataSections[i])
+            //     }
+            // }
+            // let lineOfPoints = ''
+            // for (const point of points) {
+            //     lineOfPoints += '[' + point + '],'
+            // }
+            // const trimmedLine = lineOfPoints.slice(0, -1)
+            // const encapsulatedPoints = '[' + trimmedLine + ']'
+            // props.setDataSet(encapsulatedPoints)
+        }
+        reader.readAsText(file)
+    }
     return (
         <main>
             <h1>Welcome</h1>
             <p>Use our site to get regression models for any data set you enter.</p>
+            <input type="file" style={{display: 'none'}} 
+            ref={inputFile}
+            onChange={handleFile}
+            />
+            <button onClick={handleClick}
+            >Upload</button>
+            <p id="results"></p>
         </main>
     )
 }
