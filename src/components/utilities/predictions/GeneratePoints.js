c5a69f57331cfe70e5e823eca3a99b0043f6b5d0
jtreeves <jackson.t.reeves@gmail.com>
2021-05-11 08:01:56 -0400

    [GeneratePoints] Add error handling to avoid ever having a non-positive increment

--- src/components/utilities/predictions/GeneratePoints.js
+++ src/components/utilities/predictions/GeneratePoints.js
@@ -3,6 +3,10 @@ import Evaluations from './Evaluations'
 function GeneratePoints(equationType, coefficients, minimum, maximum, increment, precision) {
     let finalPoints = []
 
+    if (increment <= 0) {
+        increment = 10**(-precision)
+    }
+
     for (let x = minimum; x <= maximum; x += increment) {
         if (
             (equationType !== ('logarithmic' || 'hyperbolic')) || 
