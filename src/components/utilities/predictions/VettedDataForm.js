e0c5ae928c1b915ee4ba44774d5869fbdd82a267
jtreeves <jackson.t.reeves@gmail.com>
2021-05-13 18:01:21 -0400

    [VettedDataForm] Add more granular error handling to ensure dataSet is an array of at least 10 arrays of exactly two numbers

--- src/components/utilities/predictions/VettedDataForm.js
+++ src/components/utilities/predictions/VettedDataForm.js
@@ -17,12 +17,35 @@ function VettedDataForm(title, independent, dependent, precision, dataSet) {
         return false
     } else {
         try {
-            console.log('ORIGINAL DATA SET: ', dataSet)
             const cleanedDataSet = CleanCollection(dataSet)
-            console.log('CLEANED DATA SET: ', cleanedDataSet)
             const parsedDataSet = JSON.parse(cleanedDataSet)
-            console.log('PARSED DATA SET: ', parsedDataSet)
-            if (parsedDataSet.length >= 10) {
+
+            if (!Array.isArray(parsedDataSet)) {
+                alert('Data set must be a list')
+                return false
+            } else {
+                for (const point of parsedDataSet) {
+                    if (!Array.isArray(point)) {
+                        alert('Each element of data set must be a coordinate pair')
+                        return false
+                    } else if (point.length !== 2) {
+                        alert('Each coordinate pair of data set must contain exactly two elements')
+                        return false
+                    } else {
+                        for (const coordinate of point) {
+                            if (!Number.isFinite(coordinate)) {
+                                alert('Each element of each coordinate pair of data set must be a number')
+                                return false
+                            }
+                        }
+                    }
+                }
+            }
+            
+            if (parsedDataSet.length < 10) {
+                alert('Data set must include at least ten points')
+                return false
+            } else {
                 const submission = {
                     title,
                     independent,
@@ -30,14 +53,11 @@ function VettedDataForm(title, independent, dependent, precision, dataSet) {
                     precision: parseInt(precision),
                     dataSet: parsedDataSet
                 }
-    
+
                 return submission
-            } else {
-                alert('You must include at least 10 points in your data set')
-                return false
             }
         } catch (error) {
-            alert('Data set must be a list of number pairs')
+            alert('A typo appears in your data set; adding or removing brackets may resolve the typo')
             return false
         }
     }
