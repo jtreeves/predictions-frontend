63755a26890a5718df3e45a9fe1b16ffc1991b24
jtreeves <jackson.t.reeves@gmail.com>
2021-05-18 01:09:28 -0400

    [CreateSession] Use try/catch with return/throw to ensure everything is passed correctly, thus fixing Login function

--- src/actions/users/CreateSession.js
+++ src/actions/users/CreateSession.js
@@ -3,10 +3,11 @@ const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
 
 async function CreateSession(userData) {
     try {
-        await axios.post(
+        const newSession = await axios.post(
             REACT_APP_SERVER_URL + 'users/login', 
             userData
         )
+        return newSession
     } catch (error) {
         throw error
     }
