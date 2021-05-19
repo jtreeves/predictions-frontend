c88d94fe44cad791de03197df3d3ae6b364d6a7e
jtreeves <jackson.t.reeves@gmail.com>
2021-05-19 17:02:58 -0400

    [Login] Import NewSessionForm

--- src/components/pages/users/Login.js
+++ src/components/pages/users/Login.js
@@ -1,80 +1,17 @@
-import { useState } from 'react'
-import { Redirect } from 'react-router-dom'
-import jwt_decode from 'jwt-decode'
-import FormItem from '../../elements/main/FormItem'
-import FormButton from '../../buttons/main/FormButton'
-import VettedLogin from '../../../utilities/users/VettedLogin'
-import CreateSession from '../../../actions/users/CreateSession'
-import Authentication from '../../../actions/main/Authentication'
+import NewSessionForm from '../../sections/users/NewSessionForm'
 import '../../../style/users/Login.css'
 
 function Login(props) {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-
-    const handleEmail = (e) => {
-        setEmail(e.target.value)
-    }
-
-    const handlePassword = (e) => {
-        setPassword(e.target.value)
-    }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const userData = VettedLogin(email, password)
-
-        if (userData) {
-            try {
-                const currentUser = await CreateSession(userData)
-                const {token} = currentUser.data
-                localStorage.setItem('jwtToken', token)
-                Authentication(token)
-                const decodedUser = jwt_decode(token)
-                props.nowCurrentUser(decodedUser)
-            } catch (error) {
-                alert(error.response.data.msg)
-                console.log(error)
-            }
-        }
-    }
-
-    if (!props.user) {
-        return (
-            <main className="login">
-                <h1>Log In</h1>
-
-                <form>
-                    <FormItem
-                        type="email"
-                        label="email"
-                        value={email}
-                        display="Email"
-                        tooltip="What is the email address associated with this account?"
-                        onChange={handleEmail}
-                    />
-
-                    <FormItem
-                        type="password"
-                        label="password"
-                        value={password}
-                        display="Password"
-                        tooltip="What is your password?"
-                        onChange={handlePassword}
-                    />
-
-                    <FormButton 
-                        text="Submit"
-                        onClick={handleSubmit}
-                        id="login-button"
-                        display="block"
-                    />
-                </form>
-            </main>
-        )
-    } else {
-        return <Redirect to="/profile" />
-    }
+    return (
+        <main className="login">
+            <h1>Log In</h1>
+            
+            <NewSessionForm 
+                user={props.user} 
+                nowCurrentUser={props.nowCurrentUser}
+            />
+        </main>
+    )
 }
 
 export default Login
