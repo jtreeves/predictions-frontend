d5e5bfa49546ee2625baad94ac75939a31124206
jtreeves <jackson.t.reeves@gmail.com>
2021-05-19 16:32:41 -0400

    [DataSets] Import ListPredictions

--- src/components/pages/predictions/DataSets.js
+++ src/components/pages/predictions/DataSets.js
@@ -1,76 +1,16 @@
-import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import GetPredictions from '../../../actions/predictions/GetPredictions'
 import CheckExpiration from '../../../utilities/users/CheckExpiration'
+import ListPredictions from '../../sections/predictions/ListPredictions'
 import '../../../style/predictions/Data.css'
 
 function DataSets(props) {
     CheckExpiration(props.user.exp, props.handleLogout)
 
-    const [predictions, setPredictions] = useState([])
-    const [loading, setLoading] = useState(true)
-    
-    const getPredictions = async () => {
-        if (props.user.id) {
-            try {
-                const rawData = await GetPredictions(props.user.id)
-                const results = rawData.data.collections.map(
-                    (collection, index) => {
-                        return (
-                            <li
-                                key={index}
-                            >
-                                <Link
-                                    to={{
-                                        pathname: "/analysis",
-                                        state: {
-                                            models: collection.regression, 
-                                            opinions: collection.prediction,
-                                            user: props.user,
-                                            initiated: true,
-                                            stored: true
-                                        }
-                                    }}
-                                    style={{ textDecoration: 'none' }} 
-                                >
-                                    <button>
-                                        {collection.regression.title}
-                                    </button>
-                                </Link>
-                            </li>
-                        )
-                    }
-                )
-                setPredictions(results.reverse())
-            } catch (error) {
-                setPredictions('')
-                console.log(error)
-            }
-        }
-        setLoading(false)
-    }
-
-    useEffect(() => {
-        getPredictions()
-    })
-
-    if (loading) {
-        return (
-            <main className="data">
-                LOADING!
-            </main>
-        )
-    } else {
-        return (
-            <main className="data">
-                <h1>Models</h1>
-
-                <ul>
-                    {predictions}
-                </ul>
-            </main>
-        )
-    }
+    return (
+        <main className="data">
+            <h1>Models</h1>
+            <ListPredictions user={props.user} />
+        </main>
+    )
 }
 
 export default DataSets
