a8f66a55cfe8b9ee9343d055327ad4588326cb0c
jtreeves <jackson.t.reeves@gmail.com>
2021-05-19 14:52:10 -0400

    [DisplayEmail] Implement useEffect and GetUser to always display the current email of the user as saved in the database

--- src/components/elements/users/DisplayEmail.js
+++ src/components/elements/users/DisplayEmail.js
@@ -1,8 +1,29 @@
+import { useEffect } from 'react'
+import GetUser from '../../../actions/users/GetUser'
+
 function DisplayEmail(props) {
+    const id = props.id
+    const email = props.email
+    const setEmail = props.setEmail
+
+    const getEmail = async () => {
+        try {
+            const currentUser = await GetUser(id)
+            setEmail(currentUser.data.user.email)
+        } catch (error) {
+            setEmail('')
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getEmail()
+    })
+
     return (
         <article>
             <h3>Email</h3>
-            <p>{props.email}</p>
+            <p>{email}</p>
         </article>
     )
 }
