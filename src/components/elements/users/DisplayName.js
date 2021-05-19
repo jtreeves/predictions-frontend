fb4ae74169b4aa6db8ce8935065d80b6a49b71d0
jtreeves <jackson.t.reeves@gmail.com>
2021-05-19 14:51:06 -0400

    [DisplayName] Implement useEffect and GetUser to always display the name of the user as currently saved in the database

--- src/components/elements/users/DisplayName.js
+++ src/components/elements/users/DisplayName.js
@@ -1,8 +1,29 @@
+import { useEffect } from 'react'
+import GetUser from '../../../actions/users/GetUser'
+
 function DisplayName(props) {
+    const id = props.id
+    const name = props.name
+    const setName = props.setName
+
+    const getName = async () => {
+        try {
+            const currentUser = await GetUser(id)
+            setName(currentUser.data.user.name)
+        } catch (error) {
+            setName('')
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getName()
+    })
+
     return (
         <article>
             <h3>Name</h3>
-            <p>{props.name}</p>
+            <p>{name}</p>
         </article>
     )
 }
