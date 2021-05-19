e8192691c6dc7d1b1dc8618941d5b9766b0b4493
jtreeves <jackson.t.reeves@gmail.com>
2021-05-19 14:49:48 -0400

    [GetUser] Use Axios to get current information on a user, like email and name, from the database

---
+++ src/actions/users/GetUser.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
+
+async function GetUser(id) {
+    try {
+        const user = await axios.get(
+            REACT_APP_SERVER_URL + 'users/' + id
+        )
+        return user
+    } catch (error) {
+        throw error
+    }
+}
+
+export default GetUser
