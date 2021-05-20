8371c88cb6d2deacd09695b0e889369da4d9db15
jtreeves <jackson.t.reeves@gmail.com>
2021-05-19 21:36:55 -0400

    [CreationBasics] Provide bulleted list of five elements that must be provided in order to create a new data set, as well as an image of an example

--- src/components/elements/main/CreationBasics.js
+++ src/components/elements/main/CreationBasics.js
@@ -1,7 +1,70 @@
 function CreationBasics() {
     return (
         <article>
-            TK
+            <p>
+                You only need to provide a few basics bits of information to generate a complete analysis.
+            </p>
+
+            <h3>Elements</h3>
+
+            <ul>
+                <li>
+                    <mark>
+                        Title
+                    </mark>
+                    
+                    <span>
+                        An overarching description of your data set (e.g., Maximize Profits)
+                    </span>
+                </li>
+
+                <li>
+                    <mark>
+                        Independent
+                    </mark>
+
+                    <span>
+                        The independent variable of your data set (e.g., units)
+                    </span>
+                </li>
+                
+                <li>
+                    <mark>
+                        Dependent
+                    </mark>
+
+                    <span>
+                        The dependent variable of your data set (e.g., dollars)
+                    </span>
+                </li>
+                
+                <li>
+                    <mark>
+                        Precision
+                    </mark>
+
+                    <span>
+                        How many digits you want to include in your results (e.g., 3 would mean your results would be rounded to the thousandth's place)
+                    </span>
+                </li>
+                
+                <li>
+                    <mark>
+                        Data Set
+                    </mark>
+
+                    <span>
+                        The actual numbers you want to analyze, presented as coordinate pairs (e.g., [[34, 57], [38, 63], [46, 72]])
+                    </span>
+                </li>
+            </ul>
+
+            <h3>Example</h3>
+
+            <img 
+                src="/images/create-new-data-set.png" 
+                alt="Create New Data Set" 
+            />
         </article>
     )
 }
