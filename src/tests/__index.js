c832fc4925587b28feb3e0983c3f9ac44970842d
jtreeves <jackson.t.reeves@gmail.com>
2021-05-22 22:45:18 -0400

    [utilities.test] Confirm VettedLogin and VettedSignup work

--- src/tests/__index.js
+++ src/tests/__index.js
@@ -1,4 +1,4 @@
 import '@testing-library/jest-dom'
 import 'regenerator-runtime/runtime'
 
-// Ran 25 passing tests in 14.176s on 05/22/21
+// Ran 27 passing tests in 13.261s on 05/22/21
--- src/tests/utilities.test.js
+++ src/tests/utilities.test.js
@@ -1,15 +1,74 @@
 import EmptyInputAlert from '../utilities/main/EmptyInputAlert'
+import VettedLogin from '../utilities/users/VettedLogin'
+import VettedSignup from '../utilities/users/VettedSignup'
 
 window.alert = jest.fn()
 
 describe('EmptyInputAlert utility', () => {
+    it('allows user to move to next stage if input field not blank', () => {
+        const checkField = EmptyInputAlert('John', 'name')
+        expect(checkField).toBe(true)
+    })
+
     it('blocks user from moving to next stage if input field blank', () => {
         const checkField = EmptyInputAlert('', 'name')
         expect(checkField).toBe(false)
     })
+})
+
+describe('VettedLogin utility', () => {
+    it('provides object of user data if no fields blank', () => {
+        const userData = VettedLogin('john@email.com', 'john1234')
+        expect(userData.email).toBe('john@email.com')
+        expect(userData.password).toBe('john1234')
+    })
     
-    it('allows user to move to next stage if input field blank', () => {
-        const checkField = EmptyInputAlert('John', 'name')
-        expect(checkField).toBe(true)
+    it('fails to provide object of user data if email field blank', () => {
+        const userData = VettedLogin('', 'john1234')
+        expect(userData).toBe(false)
+    })
+    
+    it('fails to provide object of user data if password field blank', () => {
+        const userData = VettedLogin('john@email.com', '')
+        expect(userData).toBe(false)
+    })
+})
+
+describe('VettedSignup utility', () => {
+    it('provides object of user data if no fields blank and passwords match', () => {
+        const userData = VettedSignup('John', 'john@email.com', 'john1234', 'john1234')
+        expect(userData.name).toBe('John')
+        expect(userData.email).toBe('john@email.com')
+        expect(userData.password).toBe('john1234')
+    })
+    
+    it('fails to provide object of user data if name field blank', () => {
+        const userData = VettedSignup('', 'john@email.com', 'john1234', 'john1234')
+        expect(userData).toBe(false)
+    })
+    
+    it('fails to provide object of user data if email field blank', () => {
+        const userData = VettedSignup('John', '', 'john1234', 'john1234')
+        expect(userData).toBe(false)
+    })
+    
+    it('fails to provide object of user data if password field blank', () => {
+        const userData = VettedSignup('John', 'john@email.com', '', 'john1234')
+        expect(userData).toBe(false)
+    })
+    
+    it('fails to provide object of user data if confirm password field blank', () => {
+        const userData = VettedSignup('John', 'john@email.com', 'john1234', '')
+        expect(userData).toBe(false)
+    })
+    
+    it('fails to provide object of user data if passwords do not match', () => {
+        const userData = VettedSignup('John', 'john@email.com', 'john1234', '1234john')
+        expect(userData).toBe(false)
+    })
+    
+    it('fails to provide object of user data if password too short', () => {
+        const userData = VettedSignup('John', 'john@email.com', '1234', '1234')
+        expect(userData).toBe(false)
     })
 })
