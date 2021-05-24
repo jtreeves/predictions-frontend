0e0ad31ccb936a3b6ce23fd8b4b494cdad688b3f
jtreeves <jackson.t.reeves@gmail.com>
2021-05-24 15:42:55 -0400

    [elements.test] Confirm DisplayName works

--- src/tests/elements.test.js
+++ src/tests/elements.test.js
@@ -1,12 +1,85 @@
 import { render, screen } from '@testing-library/react'
+import axios from 'axios'
+import jwt_decode from 'jwt-decode'
 import Advantages from '../components/elements/main/Advantages'
 import BestFit from '../components/elements/predictions/BestFit'
+import DisplayName from '../components/elements/users/DisplayName'
+import DisplayEmail from '../components/elements/users/DisplayEmail'
+import CreateSession from '../actions/users/CreateSession'
+import Authentication from '../actions/main/Authentication'
+
+const billData = {
+    email: 'bill@email.com',
+    password: 'bill1234'
+}
+
+let billId = ''
+
+beforeAll(async () => {
+    const currentSession = await CreateSession(billData)
+    const {token} = currentSession.data
+    localStorage.setItem('jwtToken', token)
+    Authentication(token)
+    const decodedUser = jwt_decode(token)
+    billId = decodedUser.id
+})
+
+afterAll(async () => {
+    localStorage.removeItem('jwtToken')
+    delete axios.defaults.headers.common['Authorization']
+})
 
 describe('Advantages element', () => {
     it('contains explanatory text', () => {
         render(<Advantages />)
         const article = screen.getByText(/Unlike other sites/i)
         expect(article).toBeTruthy()
+    })
+})
+
+describe('DisplayName element', () => {
+    it('displays current name stored in database', async () => {
+        let name = ''
+        const mockSetName = (element) => {
+            name = element
+        }
+        const initialNameArea = <DisplayName 
+            id={billId}
+            name={name}
+            setName={mockSetName}
+        />
+        render(initialNameArea)
+        await new Promise((c) => setTimeout(c, 1000))
+        const updatedNameArea = <DisplayName 
+            id={billId}
+            name={name}
+            setName={mockSetName}
+        />
+        render(updatedNameArea)
+        const currentName = await screen.findByText('billy boy')
+        expect(currentName).toBeTruthy()
+    })
+    
+    it('displays empty string if no ID provided', async () => {
+        let name = ''
+        const mockSetName = (element) => {
+            name = element
+        }
+        const initialNameArea = <DisplayName 
+            id=''
+            name={name}
+            setName={mockSetName}
+        />
+        render(initialNameArea)
+        await new Promise((c) => setTimeout(c, 1000))
+        const updatedNameArea = <DisplayName 
+            id=''
+            name={name}
+            setName={mockSetName}
+        />
+        render(updatedNameArea)
+        const articles = await screen.findAllByRole('article')
+        expect(articles[1].children[1].textContent).toBe('')
     })
 })
 
